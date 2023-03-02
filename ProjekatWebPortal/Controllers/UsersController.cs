62af3374b119e9234b67cf6f4f3faf7a03c5484b
Ivan <ivan.seguljev@yahoo.com>
2023-03-02 18:47:02 +0100

    Refactor

---
+++ ProjekatWebPortal/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjekatWebPortal.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Info()
+        {
+            return Content("Uspesno preusmereno na stranu info!");
+        }
+
+        public IActionResult Edit()
+        {
+            return Content("Uspesno preusmereno na stranu izmeni!");
+        }
+
+        public IActionResult Delete()
+        {
+            return Content("Uspesno preusmereno na stranu obrisi!");
+        }
+    }
+}
