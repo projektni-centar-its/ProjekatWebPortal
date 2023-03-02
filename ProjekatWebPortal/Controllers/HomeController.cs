9c5999177629de4be95d4daf37b4b71708becc17
DESKTOP-78CSA7S\Vladan <vladan19019@its.edu.rs>
2023-03-02 17:13:23 +0100

    Initial commit

--- ProjekatWebPortal/Controllers/HomeController.cs
+++ ProjekatWebPortal/Controllers/HomeController.cs
@@ -28,6 +28,21 @@ namespace ProjekatWebPortal.Controllers
             return View();
         }
 
+        public IActionResult Info()
+        {
+            return Content("Uspesno preusmereno na stranu info!");
+        }
+
+        public IActionResult Izmeni()
+        {
+            return Content("Uspesno preusmereno na stranu izmeni!");
+        }
+
+        public IActionResult Obrisi()
+        {
+            return Content("Uspesno preusmereno na stranu obrisi!");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
