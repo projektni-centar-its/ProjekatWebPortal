25b07424e51cb4f52be9482639e610acaff53231
DESKTOP-78CSA7S\Vladan <vladan19019@its.edu.rs>
2023-04-18 10:30:53 +0200

    PredmetPoSmeru

---
+++ ProjekatWebPortal/Models/Predmet.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjekatWebPortal.Models
+{
+    public class Predmet
+    {
+        [Key]
+        public int predmetId { get; set; }
+
+        [Required] 
+        public string predmetNaziv { get; set; }
+        [Required]
+        public string predmetOpis { get; set; }
+        [Required]
+        public int Razred { get; set;}
+        [Required]
+        public int tipId { get; set; }
+
+        public IList<PredmetPoSmeru> PredmetPoSmerus { get; set; }
+
+    }
+}
---
+++ ProjekatWebPortal/Models/PredmetPoSmeru.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjekatWebPortal.Models
+{
+    public class PredmetPoSmeru
+    {
+        public int PredmetId { get; set; }
+        public Predmet Predmet { get; set; }
+
+        public int SmerId { get; set; }
+        public Smer Smer { get; set; }
+
+    }
+}
